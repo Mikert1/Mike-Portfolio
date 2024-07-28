d75b731e0b6ea5872f1fde099d70299f631eba36
Mike B <mikebalvert@icloud.com>
2024-07-29 00:22:35 +0200

    love the cards for now (end of day)

--- objects/projects.js
+++ objects/projects.js
@@ -22,10 +22,17 @@ getData()
     const filteredData = data.filter(project => project.project == projectType);
         filteredData.forEach(project => {
             const clone = template.content.cloneNode(true);
-            clone.querySelector('p').textContent = project.name + ' · ' + project.type;
+            clone.querySelector('.name').textContent = project.name + ' · ' + project.type;
             clone.querySelector('h2').textContent = project.lang;
             const description = project.description.length > 90 ? project.description.slice(0, 90) + '...' : project.description;
-            clone.querySelector('h4').textContent = description;
+            clone.querySelector('.description').textContent = description;
+            const currentDate = new Date();  
+            const projectDate = new Date(project.date);
+            const timeDifference = Math.abs(currentDate - projectDate);
+            const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+            const yearsDifference = Math.floor(daysDifference / 365);
+
+            clone.querySelector('.date').textContent = `Created ${daysDifference} days ago`;
             clone.querySelector('div').addEventListener('click', () => {
                 top.location.href = `../project.html?id=${project.id}`;
             });
