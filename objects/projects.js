95f973ad24d87ca6f6efa4f0594339a374f62b64
Mike B <mikebalvert@icloud.com>
2024-07-30 00:12:39 +0200

    makeing cards better and better also updated project data like dates and status

--- objects/projects.js
+++ objects/projects.js
@@ -22,17 +22,16 @@ getData()
     const filteredData = data.filter(project => project.project == projectType);
         filteredData.forEach(project => {
             const clone = template.content.cloneNode(true);
-            clone.querySelector('.name').textContent = project.name + ' · ' + project.type;
-            clone.querySelector('h2').textContent = project.lang;
+            clone.querySelector('.name').innerHTML = '<span>' + project.name + ' · ' + project.type + '</span>';
+            clone.querySelector('h2').innerHTML = '<span>' + project.lang + '</span>';
             const description = project.description.length > 90 ? project.description.slice(0, 90) + '...' : project.description;
             clone.querySelector('.description').textContent = description;
             const currentDate = new Date();  
             const projectDate = new Date(project.date);
             const timeDifference = Math.abs(currentDate - projectDate);
             const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-            const yearsDifference = Math.floor(daysDifference / 365);
 
-            clone.querySelector('.date').textContent = `Created ${daysDifference} days ago`;
+            clone.querySelector('.date').innerHTML = `Created <span>${daysDifference}</span> days ago | <span>${project.status}</span>`;
             clone.querySelector('div').addEventListener('click', () => {
                 top.location.href = `../project.html?id=${project.id}`;
             });
