74abbf6324d13e0a0a57129368f8f113fd5a0b75
M Balvert <mikebalvert@icloud.com>
2024-08-11 14:05:12 +0200

    better mobile view for programs info

--- script.js
+++ script.js
@@ -9,7 +9,20 @@ if (hr < 12) {
 }
 helloText.innerHTML += ', Welcome to my website!';
 
-async function getData() {
+async function getProjects() {
+    try {
+        const response = await fetch('projects.json');
+        if (!response.ok) {
+            throw new Error('Failed to fetch');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching:', error);
+        throw error;
+    }
+}
+async function getPrograms() {
     try {
         const response = await fetch('projects.json');
         if (!response.ok) {
@@ -36,7 +49,17 @@ function flipCard(side) {
 
 let myOwnProjectsCount = 0;
 let schoolProjectsCount = 0;
-getData()
+let programsCount = 0;
+getPrograms()
+    .then(data => {
+        programsCount = data.length;
+        console.log(`Number of programs: ${programsCount}`);
+    })
+    .catch(error => {
+        console.error('Error fetching:', error);
+    });
+
+getProjects()
     .then(data => {
         const gameProjects = data.filter(project => project.project === 'solo');
         myOwnProjectsCount = gameProjects.length;
@@ -54,11 +77,14 @@ function resize() {
     if (window.innerWidth <= 500) {
         const num = myOwnProjectsCount * 450 + 10;
         const num2 = schoolProjectsCount * 450 + 10;
+        const num3 = programsCount * 100 + 10;
         document.getElementById('myOwnProjects').height = num;
         document.getElementById('schoolProjects').height = num2;
+        document.getElementById('programs-object').height = num3;
     } else {
         document.getElementById('myOwnProjects').height = '450px';
         document.getElementById('schoolProjects').height = '450px';
+        document.getElementById('programs-object').height = '150px';
     }
 }
 
