a3d671062b81ae68f56f13456f9f5e6351da39e1
M Balvert <mikebalvert@icloud.com>
2024-08-24 20:31:15 +0200

    changed card at start and removed some things

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let hr = (new Date()).getHours();
-const helloText = document.getElementById('welcome');
-if (hr < 12) {
-    helloText.innerHTML = 'A very Good Morning';
-} else if (hr < 18) {
-    helloText.innerHTML = 'A very Good Afternoon';
-} else {
-    helloText.innerHTML = 'A very Good Evening';
-}
-helloText.innerHTML += ', Welcome to my website!';
+// const helloText = document.getElementById('welcome');
+// if (hr < 12) {
+//     helloText.innerHTML = 'A very Good Morning';
+// } else if (hr < 18) {
+//     helloText.innerHTML = 'A very Good Afternoon';
+// } else {
+//     helloText.innerHTML = 'A very Good Evening';
+// }
+// helloText.innerHTML += ', Welcome to my website!';
 
 async function getProjects() {
     try {
