2f1b53d244d51a06dd014031e057791fd643420d
M Balvert <mikebalvert@icloud.com>
2024-10-05 15:48:44 +0200

    added display for ammount of certifacates

--- objects/languages.js
+++ objects/languages.js
@@ -1,8 +1,8 @@
 const template = document.getElementById('template');
 
-async function getData() {
+async function getData(file) {
     try {
-        const response = await fetch('../languages.json');
+        const response = await fetch(`../${file}.json`);
         if (!response.ok) {
             throw new Error('Failed to fetch');
         }
@@ -14,24 +14,32 @@ async function getData() {
     }
 }
 
-getData()
+
+
+getData("languages")
 .then(data => {
     data.forEach(Langs => {
-        const clone = template.content.cloneNode(true);
-        clone.querySelector('.name').textContent = Langs.name;
-        clone.querySelector('.image').src = `../assets/img/languages/${Langs.name}.png`;
-        clone.querySelector('.description').textContent = Langs.description;
-        const currentDate = new Date();
-        const projectDate = new Date(Langs.since);
-        const timeDifference = Math.abs(currentDate - projectDate);
-        const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+        getData("certificates").then(certificates => {
+            const clone = template.content.cloneNode(true);
+            clone.querySelector('.name').textContent = Langs.name;
+            clone.querySelector('.image').src = `../assets/img/languages/${Langs.name}.png`;
+            clone.querySelector('.description').textContent = Langs.description;
+            const currentDate = new Date();
+            const projectDate = new Date(Langs.since);
+            const timeDifference = Math.abs(currentDate - projectDate);
+            const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
 
-        if (daysDifference >= 730) {
-            const yearsSince = Math.floor(daysDifference / 365);
-            clone.querySelector('.experience').textContent = yearsSince + ' year' + '+ experience';
-        } else {
-            clone.querySelector('.experience').textContent = daysDifference + ' days experience';
-        }
-        document.getElementById('languages').appendChild(clone);
+            if (daysDifference >= 730) {
+                const yearsSince = Math.floor(daysDifference / 365);
+                clone.querySelector('.experience').textContent = yearsSince + ' year' + '+ experience';
+            } else {
+                clone.querySelector('.experience').textContent = daysDifference + ' days experience';
+            }
+
+            const langCertificates = certificates.filter(cert => cert.part_of.includes(Langs.name));
+            clone.querySelector('.certificates').textContent = langCertificates.length + ' certificates';
+            
+            document.getElementById('languages').appendChild(clone);
+        });
     });
 });
