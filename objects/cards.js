0ddb80a0c582c87ac9ea1adf0d80cfa4e49d4d62
Mike B <mikebalvert@icloud.com>
2024-11-03 20:13:48 +0100

    updated and renamed mikert video > cryonium library

--- objects/cards.js
+++ objects/cards.js
@@ -53,7 +53,7 @@ getData()
         const contributorsLength = Object.keys(project.contributors).length;
         clone.querySelector('.team').innerHTML = contributorsLength > 1 ? `Team of <span>${contributorsLength}</span>` : `<span>Solo</span> project`;
         clone.querySelector('.link').href = `/project/?id=${project.id}`;
-        clone.querySelector('img').src = `${project.images[0]}`;
+        clone.querySelector('img').src = `../assets/projects/${project.name}/1.png`;
         document.getElementById('projects').appendChild(clone);
     });
 });
--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let hr = (new Date()).getHours();
-// const helloText = document.getElementById('welcome');
-// if (hr < 12) {
-//     helloText.innerHTML = 'A very Good Morning';
-// } else if (hr < 18) {
-//     helloText.innerHTML = 'A very Good Afternoon';
-// } else {
-//     helloText.innerHTML = 'A very Good Evening';
-// }
-// helloText.innerHTML += ', Welcome to my website!';
-
 async function getProjects() {
     try {
         const response = await fetch('projects.json');
