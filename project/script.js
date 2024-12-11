a9f7967246f394787e58479d96be43339114c298
Mike Balvert <mikebalvert@icloud.com>
2024-12-11 13:52:55 +0100

    added stack info and changed order of imformatrion

--- project/script.js
+++ project/script.js
@@ -70,119 +70,122 @@ async function getWebsiteStatus(url) {
 async function setProject() {
     data = await getData();
     const project = data[params.id];
-        title.textContent = project.name;
-        const logo = document.getElementById('logo');
-        logo.src = `../assets/projects/${project.name}/logo.png`;
-        description.textContent = project.description;
-        if (project.platforms) {
-            for (let i = 0; i < project.platforms.length; i++) {
-                fetch(`../assets/img/svg/${project.platforms[i]}.svg`)
-                .then(response => response.text())
-                .then(data => {
-                    const div = document.createElement('div');
-                    div.innerHTML = data;
-                    platforms.appendChild(div);
-                });
-            }
-        }
-        if (project.type === 'Website') {
-            const output = await getWebsiteStatus(project.link);
-            const spanElement = document.createElement('span');
-            if (output == true) {
-                spanElement.textContent = 'Online';
-                spanElement.classList.add('statusDone');
-            } else {
-                spanElement.textContent = 'Offline';
-                spanElement.classList.add('statusInactive');
-            }
-            status.appendChild(spanElement);
-        } else {
-            status.style.display = 'none';
-        }
-        date.textContent = project.date;
-        const langEntries = Object.keys(project.lang);
-        langEntries.forEach((n, index) => {
-            const dot = index < langEntries.length - 1 ? ', ' : ' ';
-            const span = document.createElement('span');
-            span.textContent = n;
-            lang.appendChild(span);
-            lang.innerHTML += dot;
-            if (project.lang[n].Lines) {
-                const p = document.createElement('p'); p.classList.add('m0');
-                p.textContent = project.lang[n].Lines + ' lines';
-                span.appendChild(p);
-            }
-            if (project.lang[n].Files) {
-                const p = document.createElement('p'); p.classList.add('m0');
-                p.textContent = project.lang[n].Files + ' files';
-                span.appendChild(p);
-            }
-            moreLang.appendChild(span);
-        });
-        for (let i = 0; i < 5; i++) {
-            const div = document.createElement('div');
-            let img = document.createElement('img');
-            img.src = `../assets/projects/${project.name}/${i + 1}.png`;
-            img.id = `image${i + 1}`;
-            div.appendChild(img);
-            document.querySelector('.sub-images').appendChild(div);
-            document.title = "Mikert.com | " + project.name;
-        }
-        mainImage.src = `../assets/projects/${project.name}/1.png`;
-        const svg = document.getElementById("svg");
-        if (project.type === 'Game') {
-            const download = document.getElementById('mainButton');
-            let osName = window.navigator.platform;
-            if (osName.includes('Win')) {
-                osName = '../assets/img/svg/windows.svg';
-            } else if (osName.includes('Mac')) {
-                osName = '../assets/img/svg/mac.svg';
-            } else if (osName.includes('Linux')) {
-                osName = '../assets/img/svg/linux.svg';
-            } else {
-                osName = '../assets/img/svg/download.svg';
-            }
-            fetch(osName)
-                .then(response => response.text())
-                .then(data => {
-                    svg.innerHTML = data;
-                });
-            download.querySelector('p').innerHTML = 'Download';
-            download.href = project.releases;
-            version.innerHTML = version.innerHTML + '<p>' + project.versionPrefix + '<span>' + project.version + '</span>' + project.versionSuffix + '</p>';
-        } else if (project.type === 'Website') {
-            const link = document.getElementById('mainButton');
-            link.querySelector('p').innerHTML = 'Visit Website';
-            link.href = project.link;
-            version.innerHTML = "";
-            svg.style.display = 'none';
-        }
-        const contributorEntries = Object.keys(project.contributors);
-        contributorEntries.forEach((n, index) => {
-            const img = document.createElement('img');
-            img.src = project.contributors[n].image;
-            contributors.appendChild(img);
-
-            const div = document.createElement('div');
-            const innerDiv = document.createElement('div'); innerDiv.classList.add('flex-ai-center', 'gap');
-            const img2 = document.createElement('img'); 
-            img2.src = project.contributors[n].image;
-            innerDiv.appendChild(img2);
-            innerDiv.innerHTML += n;
-            div.appendChild(innerDiv);
-            if (project.contributors[n].role) {
-                div.innerHTML += ' - ' + project.contributors[n].role;
-            }
-            moreContributors.appendChild(div);
-        });
-        const source = document.getElementById('source');
-        source.href = project.repository;
-        const subImages = document.querySelectorAll('.sub-images div img');
-        subImages.forEach((image) => {
-            image.addEventListener('click', (e) => {
-                document.getElementById('main-image').src = e.target.src;
+    title.textContent = project.name;
+    const logo = document.getElementById('logo');
+    logo.src = `../assets/projects/${project.name}/logo.png`;
+    description.textContent = project.description;
+    if (project.platforms) {
+        for (let i = 0; i < project.platforms.length; i++) {
+            fetch(`../assets/img/svg/${project.platforms[i]}.svg`)
+            .then(response => response.text())
+            .then(data => {
+                const div = document.createElement('div');
+                div.innerHTML = data;
+                platforms.appendChild(div);
             });
+        }
+    }
+    if (project.type === 'Website') {
+        const output = await getWebsiteStatus(project.link);
+        const spanElement = document.createElement('span');
+        if (output == true) {
+            spanElement.textContent = 'Online';
+            spanElement.classList.add('statusDone');
+        } else {
+            spanElement.textContent = 'Offline';
+            spanElement.classList.add('statusInactive');
+        }
+        status.appendChild(spanElement);
+    } else {
+        status.style.display = 'none';
+    }
+    date.textContent = project.date;
+    const langEntries = Object.keys(project.lang);
+    langEntries.forEach((n, index) => {
+        const dot = index < langEntries.length - 1 ? ', ' : ' ';
+        const span = document.createElement('span');
+        span.textContent = n;
+        lang.appendChild(span);
+        lang.innerHTML += dot;
+        if (project.lang[n].Lines) {
+            const p = document.createElement('p'); p.classList.add('m0');
+            p.textContent = project.lang[n].Lines + ' lines';
+            span.appendChild(p);
+        }
+        if (project.lang[n].Files) {
+            const p = document.createElement('p'); p.classList.add('m0');
+            p.textContent = project.lang[n].Files + ' files';
+            span.appendChild(p);
+        }
+        moreLang.appendChild(span);
+    });
+    for (let i = 0; i < 5; i++) {
+        const div = document.createElement('div');
+        let img = document.createElement('img');
+        img.src = `../assets/projects/${project.name}/${i + 1}.png`;
+        img.id = `image${i + 1}`;
+        div.appendChild(img);
+        document.querySelector('.sub-images').appendChild(div);
+        document.title = "Mikert.com | " + project.name;
+    }
+    mainImage.src = `../assets/projects/${project.name}/1.png`;
+    const svg = document.getElementById("svg");
+    if (project.type === 'Game') {
+        const download = document.getElementById('mainButton');
+        let osName = window.navigator.platform;
+        if (osName.includes('Win')) {
+            osName = '../assets/img/svg/windows.svg';
+        } else if (osName.includes('Mac')) {
+            osName = '../assets/img/svg/mac.svg';
+        } else if (osName.includes('Linux')) {
+            osName = '../assets/img/svg/linux.svg';
+        } else {
+            osName = '../assets/img/svg/download.svg';
+        }
+        fetch(osName)
+            .then(response => response.text())
+            .then(data => {
+                svg.innerHTML = data;
+            });
+        download.querySelector('p').innerHTML = 'Download';
+        download.href = project.releases;
+        version.innerHTML = version.innerHTML + '<p>' + project.versionPrefix + '<span>' + project.version + '</span>' + project.versionSuffix + '</p>';
+    } else if (project.type === 'Website') {
+        const link = document.getElementById('mainButton');
+        link.querySelector('p').innerHTML = 'Visit Website';
+        link.href = project.link;
+        version.innerHTML = "";
+        svg.style.display = 'none';
+
+    }
+    const stack = document.getElementById('stack');
+    stack.innerHTML = project.stack;
+    const contributorEntries = Object.keys(project.contributors);
+    contributorEntries.forEach((n, index) => {
+        const img = document.createElement('img');
+        img.src = project.contributors[n].image;
+        contributors.appendChild(img);
+
+        const div = document.createElement('div');
+        const innerDiv = document.createElement('div'); innerDiv.classList.add('flex-ai-center', 'gap');
+        const img2 = document.createElement('img'); 
+        img2.src = project.contributors[n].image;
+        innerDiv.appendChild(img2);
+        innerDiv.innerHTML += n;
+        div.appendChild(innerDiv);
+        if (project.contributors[n].role) {
+            div.innerHTML += ' - ' + project.contributors[n].role;
+        }
+        moreContributors.appendChild(div);
+    });
+    const source = document.getElementById('source');
+    source.href = project.repository;
+    const subImages = document.querySelectorAll('.sub-images div img');
+    subImages.forEach((image) => {
+        image.addEventListener('click', (e) => {
+            document.getElementById('main-image').src = e.target.src;
         });
+    });
 }
 
 setProject();
